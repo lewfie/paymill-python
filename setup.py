2f9092b5772b57d11c89e6c055e0eb70cb9cba04
Aleksandar Yalnazov <yalnazov@gmail.com>
2014-12-15 13:24:34 +0200

    Setup.py enhanced for PyPi upload. MANIFEST.in added.

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='paymill',
     version='1.1.0',
     description='Python wrapper for PAYMILL API',
+    author='Aleksandar Yalnazov',
+    author_email='aleksandar.yalnazov@qaiware.com',
+    url='https://github.com/paymill/paymill-python',
+    license='MIT',
     packages=['paymill'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -11,6 +15,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
         'License :: OSI Approved :: MIT License',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
